2e19cccac4354da11a57c596cb8dac9009279ae3
Devin Moorjani <clarkkent4027@gmail.com>
2024-10-06 10:43:12 -0700

    Done

--- src/components/SearchOutput/SearchResult/SearchResult.js
+++ src/components/SearchOutput/SearchResult/SearchResult.js
@@ -2,15 +2,12 @@ import React from "react";
 import classes from './SearchResult.module.css';
 
 const SearchResult = (props) => {
-    console.log(props);
+    console.log("props",props);
     return (
         <article className={classes.ElephantDTO}>
-            <h1>Title: {props.word}</h1>
-            <h3 className={classes.Meaning}>
-                {props.meaning && props.meaning.map((meaning, index) => (
-                    <p key={index}>{meaning}</p>
-                ))}
-            </h3>
+            <h1>Definition: </h1>
+           <p><b>{props.definition}</b></p>
+           <p>{props.example}</p>
             <a href={props.link} rel="noopener noreferrer" target="_blank">{props.link}</a>
         </article>
     );
